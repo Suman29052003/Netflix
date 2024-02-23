c5058cdafa3d8dc8b9455ef90c306d3ea15d2bec
SumanSaha <8759127658itisaha@gmail.com>
2024-02-23 20:44:58 +0530

    Added CSS styles for the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import heroImg from "../public/images/hero.jpg";
 import logo from "../public/images/netflix-logo.png";
-
+import "./App.css"
 const App = () => {
   return (
     <>
@@ -10,7 +10,6 @@ const App = () => {
           {/* Hero Section  */}
 
         <div className={`hero w-full h-screen flex flex-col items-center `}>
-          {/* <img src={heroImg} alt="" className='w-full h-[90vh] object-cover flex flex-col'/> */}
 
           {/* NavBar  */}
           <div className="navbar w-full  flex justify-around">         
@@ -28,18 +27,37 @@ const App = () => {
           </div>
 
           {/* heading  */}
-
           <div className=" w-full text-center text-white h-[80vh] flex flex-col justify-center items-center ">
             <h4 className='font-bold  text-5xl py-2'>Unlimited movies, TV shows and more</h4>
             <p className="py-2 text-xl font-semibold">Watch anywhere. Cancel anytime.</p>
             <p className="py-2 text-xl font-semibold">Ready to watch? Enter your email to create or restart your membership.</p>
             <div className="email w-full flex items-center justify-center">
-              <input type="email" className="w-[25%] bg-transparent border-2 border-gray-600 p-4 m-2 rounded-sm" placeholder='Email Address' />
+              <input type="email" className="w-[25%] bg-gray-800 bg-opacity-50 border-2 border-gray-600 p-4 m-2 rounded-sm" placeholder='Email Address' />
               <div className="bg-red-700 p-4 m-2 rounded-sm w-[13%] text-2xl hover:bg-red-800 duration-900 cursor-pointer"><p className='w-full flex  items-center justify-center'>Get Started <span class="material-symbols-outlined  text-2xl font-normal">chevron_right</span></p></div>
             </div>
           </div>
+        </div>
+
+      {/*Second Div*/}
+      <div className="h-[80vh]  border-t-[8px] border-b-[8px] border-gray-500 flex  w-full justify-center ">
+
+        <div className="md:w-[80%]  flex md:flex-row flex-col w-full">
+
+          <div className="text-white md:w-1/2 flex flex-col justify-center md:items-left items-center w-full">
+            <p className="text-3xl mt-4 md:mt-0 md:text-5xl font-bold py-4 md:w-full text-left">Enjoy on your TV</p>
+            <p className="text-xl p-2 md:py-2 md:text-left font-semibold text-center">Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
+          </div>
+
+        <div className="md:w-1/2 md:m-0 mt-5 ">
+
+        <img alt="" src="" class="" />
 
         </div>
+
+
+        </div>
+      </div>
+
       </div>
     </>
   )
